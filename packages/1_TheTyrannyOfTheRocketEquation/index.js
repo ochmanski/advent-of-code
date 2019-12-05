b772892824e71a361b136f7aecf9aa9000d163ad
ochmanski <ochmanskikacper@gmail.com>
2019-12-05 12:45:16 +0100

    feat(1): solve second part

--- packages/1_TheTyrannyOfTheRocketEquation/index.js
+++ packages/1_TheTyrannyOfTheRocketEquation/index.js
@@ -6,10 +6,23 @@ const sum = (a, b) => a + b;
 const computeFuelRequiredForLaunch = (moduleMass) =>
   Math.floor(moduleMass / 3) - 2;
 
+const computeFuelRequirement = function*(fuel) {
+  const fuelRequirement = computeFuelRequiredForLaunch(fuel);
+
+  if (fuelRequirement <= 0) {
+    return fuel;
+  }
+
+  yield fuelRequirement;
+  yield* computeFuelRequirement(fuelRequirement);
+};
+
 const computeTotalFuelRequirement = async () => {
   const moduleMasses = (await getTextFile('input.txt')).split('\n');
 
-  return moduleMasses.map(computeFuelRequiredForLaunch).reduce(sum, 0);
+  return moduleMasses
+    .map((mass) => Array.from(computeFuelRequirement(mass)).reduce(sum, 0))
+    .reduce(sum, 0);
 };
 
 computeTotalFuelRequirement().then((fuel) => {
